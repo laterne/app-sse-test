c62cae8dff20b3261aed0e605172acaa32123585
Sebastian Kreis <sebastian.kreis@akrs.ch>
2020-09-06 15:14:58 +0200

    fix2

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ var evtSource = new EventSource('sse'),
   list = document.getElementById('sselist')
 
 evtSource.onmessage = function(e) {
-  var date = new Date();
+  var date = new Date(),
+    time = date.toLocaleTimeString();
 
-  console.log(e.data, date.getSeconds())
+  console.log(e.data, time)
 }
